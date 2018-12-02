5acc7d4d0017eaa018d907859792be700a203ef1
BeefZong <thinkfatee@gmail.com>
2018-12-01 23:51:25 -0600

    H test update

--- src/main.cpp
+++ src/main.cpp
@@ -14,35 +14,35 @@ int main(int argc, char* argv[]) {
   qmlRegisterRevision<QWindow, 1>("RCProject", 1, 0);
   qmlRegisterRevision<QQuickWindow, 1>("RCProject", 1, 0);
 
-  //
-  CCData* cc = CCData::LoadPLYFile("D:/WORK/GitHub/RootClassification/dat/test.ply");
-  cc->flatten();
-  Classification cls(cc);
-  cls.updateContainings();
-  for(uint i = 0;i<cc->n1;i++){
-      qDebug("Point %d: ",i);
-      qDebug("Edge: ");
-      for(int j = 0;j<cls.edgeContainingL[i];j++){
-          qDebug("%d ", cls.edgeContaining[i][j]);
-      }
-      qDebug("Face: ");
-      for(int j = 0;j<cls.faceContainingL[i];j++){
-          qDebug("%d ", cls.faceContaining[i][j]);
-      }
-  }
-  cls.junctionAutoDetection();
-  qDebug() << cls.junctions[0].pos;
-  qDebug() << cls.junctions[1].pos;
+//  //
+//  CCData* cc = CCData::LoadPLYFile("D:/WORK/GitHub/RootClassification/dat/test.ply");
+//  cc->flatten();
+//  Classification cls(cc);
+//  cls.updateContainings();
+//  for(uint i = 0;i<cc->n1;i++){
+//      qDebug("Point %d: ",i);
+//      qDebug("Edge: ");
+//      for(int j = 0;j<cls.edgeContainingL[i];j++){
+//          qDebug("%d ", cls.edgeContaining[i][j]);
+//      }
+//      qDebug("Face: ");
+//      for(int j = 0;j<cls.faceContainingL[i];j++){
+//          qDebug("%d ", cls.faceContaining[i][j]);
+//      }
+//  }
+//  cls.junctionAutoDetection();
+//  qDebug() << cls.junctions[0].pos;
+//  qDebug() << cls.junctions[1].pos;
 
-  // set radius to 1/4
-  cls.setJunctionRadius(0.25);
-  qDebug() << cls.junctions[0].radius;
-  qDebug() << cls.junctions[1].radius;
-  //
+//  // set radius to 1/4
+//  cls.setJunctionRadius(0.25);
+//  qDebug() << cls.junctions[0].radius;
+//  qDebug() << cls.junctions[1].radius;
+//  //
 
-  //mark points in spheres
-  cls.markVerticesInJunctionSpheres();
-  cls.classify();
+//  //mark points in spheres
+//  cls.markVerticesInJunctionSpheres();
+//  cls.classify();
 
   QQmlApplicationEngine engine;
   engine.load(QUrl("qrc:src/main.qml"));
