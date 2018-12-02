f4b67fecc587c629c5c41989be27a3e76861d73b
BeefZong <thinkfatee@gmail.com>
2018-12-02 09:47:05 -0600

    H test

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,18 @@ int main(int argc, char* argv[]) {
   qmlRegisterRevision<QWindow, 1>("RCProject", 1, 0);
   qmlRegisterRevision<QQuickWindow, 1>("RCProject", 1, 0);
 
-//  //
-//  CCData* cc = CCData::LoadPLYFile("D:/WORK/GitHub/RootClassification/dat/test.ply");
+  //
+//  CCData* cc = CCData::LoadPLYFile("D:/WORK/GitHub/RootClassification/dat/2.ply");
 //  cc->flatten();
 //  Classification cls(cc);
 //  cls.updateContainings();
 //  for(uint i = 0;i<cc->n1;i++){
 //      qDebug("Point %d: ",i);
-//      qDebug("Edge: ");
+//      qDebug("      Edge: ");
 //      for(int j = 0;j<cls.edgeContainingL[i];j++){
 //          qDebug("%d ", cls.edgeContaining[i][j]);
 //      }
-//      qDebug("Face: ");
+//      qDebug("              Face: ");
 //      for(int j = 0;j<cls.faceContainingL[i];j++){
 //          qDebug("%d ", cls.faceContaining[i][j]);
 //      }
@@ -33,16 +33,22 @@ int main(int argc, char* argv[]) {
 //  cls.junctionAutoDetection();
 //  qDebug() << cls.junctions[0].pos;
 //  qDebug() << cls.junctions[1].pos;
+//  qDebug() << cls.junctions[2].pos;
+//  cls.junctions.removeAt(0);
 
-//  // set radius to 1/4
+  // set radius to 1/4
 //  cls.setJunctionRadius(0.25);
 //  qDebug() << cls.junctions[0].radius;
 //  qDebug() << cls.junctions[1].radius;
-//  //
+  //
 
-//  //mark points in spheres
+  //mark points in spheres
 //  cls.markVerticesInJunctionSpheres();
 //  cls.classify();
+
+//    for(uint i = 0;i<cc->n1;i++){
+//        qDebug("Point %d: %d",i,cls.mark[i]);
+//    }
 
   QQmlApplicationEngine engine;
   engine.load(QUrl("qrc:src/main.qml"));
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5,17 +5,26 @@
 MainWindow::MainWindow(QWindow* p) : QQuickWindow(p), current(nullptr) {
   connect(this, &QQuickWindow::beforeRendering, this, &MainWindow::init,
           Qt::DirectConnection);
-  current = CCData::LoadPLYFile(":/dat/1.ply");
+  current = CCData::LoadPLYFile(":/dat/2.ply");
   cls = new Classification(current);
-  //  cls->classify();
+  cls->updateContainings();
+  cls->junctionAutoDetection();
+  for(int i = 0;i<this->cls->junctions.size();i++)
+  qDebug() << cls->junctions[i].pos;
+
+  cls->junctions.removeAt(2);
+  cls->setJunctionRadius(0.25);
+  cls->markVerticesInJunctionSpheres();
+  cls->classify();
 }
 
 void MainWindow::init() {
   disconnect(this, &QQuickWindow::beforeRendering, this, &MainWindow::init);
   viewer = findChild<CCViewer*>();
   viewer->setRaw(current);
-  char* m = new char[current->n1];
-  for (uint i = 0; i < current->n1; i++) m[i] = rand() % 2;
-  viewer->setMark(m);
+//  char* m = new char[current->n1];
+//  for (uint i = 0; i < current->n1; i++) m[i] = rand() % 2;
+//  viewer->setMark(m);
+  viewer->setMark(cls->mark);
   return;
 }
